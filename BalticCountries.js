f8f80f5e49790bebd74bbe2bf6f44f635a96d05b
Elena Kononchuk <eva.kononchuk@gmail.com>
2023-08-05 22:52:06 +0300

    Create BalticCountries.js

---
+++ BalticCountries.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function BalticCountries() {
+  return (
+    <ul>
+      <li>Estonia</li>
+      <li>Lanvia</li>
+      <li>Lithuania</li>
+    </ul>
+  );
+}
